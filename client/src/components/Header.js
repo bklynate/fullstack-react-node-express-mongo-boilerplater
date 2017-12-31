83a64514d88fc0b9bc466268283233acb4443a18
bklynate <nathanielcfa@gmail.com>
2017-12-31 03:08:31 -0500

    initial commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,37 @@
+/* eslint-disable consistent-return */
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/google">Log in with Google</a>;
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="header nav-wrapper">
+          <Link
+            to={this.props.auth ? '/' : '/'}
+            className="left brand-logo">
+            App Name Here
+          </Link>
+          <ul className="right header-right">
+            <li className="header-right-content">{this.renderContent()}</li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ auth: state.auth });
+
+export default connect(mapStateToProps)(Header);
---
+++ routes/authRoutes.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (request, response) => {
+      response.redirect('/searchAnime');
+    }
+  );
+
+  app.get('/api/logout', (request, response) => {
+    request.logout();
+    response.redirect('/');
+  });
+
+  app.get('/api/current_user', (request, response) => {
+    response.send(request.user);
+  });
+};
